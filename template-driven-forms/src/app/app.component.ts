0b1bbd292d2f56e28df389250f7478de7e4449c8
leekanth <leekanth9@gmail.com>
2018-11-05 16:13:28 +0530

    Initial commit

---
+++ template-driven-forms/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  topics1 = ['Single Room', 'double Room', 'Trible Room'];
+  topics2 = ['Garden View', 'Sea view', 'Fountain View'];
+  topics3 = ['Standard', 'Deluxe', 'Luxury'];
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('Rob', 'rob@test.com', 5556665566, 'default', 'morning', true);
+  topicHasError = true;
+  submitted = false;
+  errorMsg = '';
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+onSubmit() {
+ this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        response => console.log('Success!', response),
+        error => this.errorMsg = error.statusText
+
+      )
+
+    
+  }
+
+}
